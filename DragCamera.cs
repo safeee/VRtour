08303165be96472ca3fa5ba8cc9fca518d5b8823
safeee <chaouch.safee@gmail.com>
2020-05-24 20:38:47 +0100

    Update DragCamera.cs

--- DragCamera.cs
+++ DragCamera.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System;
-public class VRCamera : MonoBehaviour
+public class DragCamera : MonoBehaviour
 { // flag to keep track whether we are dragging or not
   public  bool isDragging = false;
 
